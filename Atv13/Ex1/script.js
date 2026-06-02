7abe50349ee0d05a8f76d08b349d2cc4495ec9aa
Erick <eshinfuku@gmail.com>
2026-06-02 19:32:01 -0300

    Finalizando a Atividade 13

---
+++ Atv13/Ex1/script.js
@@ -0,0 +1,8 @@
+const botao = document.getElementById('btn');
+
+function dispararAlerta() {
+    alert("Olá! Estou aprendendo JavaScript");
+}
+
+botao.addEventListener('click', dispararAlerta);
+
---
+++ Atv13/Ex2/script.js
@@ -0,0 +1,7 @@
+let nome = "Jõao"
+let idade = 20
+let cidade = "Londrina"
+
+console.log(`Nome: ${nome}`);
+console.log(`Idade: ${idade}`);
+console.log(`Cidade: ${cidade}`);
